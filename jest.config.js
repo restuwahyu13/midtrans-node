4866ec4254fccaf7ac02606df885e636e76cec36
restuwahyu13 <restuwahyu13@gmail.com>
2021-01-10 13:33:32 +0700

    publish package

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
-  testMatch: ['<rootDir>/tests/**/*.{test.ts, spec.ts}', '<rootDir>/__tests__/**/*.{test.ts, spec.ts}'],
-  collectCoverageFrom: ['src/libs/**/*.ts'],
-  testPathIgnorePatterns: ['node_modules/', '__test__/', 'dist/', 'esm', 'tsconfig.json'],
-  coveragePathIgnorePatterns: ['node_modules/', '__test__/', 'dist/', 'esm/', 'tsconfig.json']
+	preset: 'ts-jest',
+	testMatch: ['<rootDir>/tests/**/*.{test.ts, spec.ts}', '<rootDir>/__tests__/**/*.{test.ts, spec.ts}'],
+	collectCoverageFrom: ['src/libs/**/*.ts'],
+	testPathIgnorePatterns: ['node_modules/', '__test__/', 'dist/', 'esm', 'tsconfig.json'],
+	coveragePathIgnorePatterns: ['node_modules/', '__test__/', 'dist/', 'esm/', 'tsconfig.json']
 }
