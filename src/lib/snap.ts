f64bf8b1c102b428b05e604220111daa3bd88fb9
restuwahyu13 <restuwahyu13@gmail.com>
2021-01-14 20:30:20 +0700

    update snap

--- src/lib/snap.ts
+++ src/lib/snap.ts
@@ -1,47 +1,61 @@
-// 'use strict'
+import { ApiConfig } from './apiConfig'
+import { HttpClient } from './httpClient'
+import { Transaction } from './transaction'
+import { TransactionRequestType, SnapOptions } from '../types/snap'
 
-// const ApiConfig = require('./apiConfig')
-// const HttpClient = require('./httpClient')
-// const Transaction = require('./transaction')
-// /**
-//  * Snap object used to do request to Midtrans Snap API
-//  */
+/**
+ * Snap object used to do request to Midtrans Snap API
+ */
 
-// export class Snap {
-// 	constructor(options = { isProduction: false, serverKey: '', clientKey: '' }) {
-// 		this.apiConfig = new ApiConfig(options)
-// 		this.httpClient = new HttpClient(this)
-// 		this.transaction = new Transaction(this)
-// 	}
+export class Snap {
+	public apiConfig: ApiConfig
+	public httpClient: HttpClient
+	public transaction: Transaction
 
-// 	/**
-// 	 * Do `/transactions` API request to Snap API
-// 	 * @param  {Object} parameter - object of Core API JSON body as parameter, will be converted to JSON (more params detail refer to: https://snap-docs.midtrans.com)
-// 	 * @return {Promise} - Promise contains Object from JSON decoded response
-// 	 */
-// 	createTransaction(parameter = {}) {
-// 		let apiUrl = this.apiConfig.getSnapApiBaseUrl() + '/transactions'
-// 		let responsePromise = this.httpClient.request('post', this.apiConfig.get().serverKey, apiUrl, parameter)
-// 		return responsePromise
-// 	}
+	constructor(options: SnapOptions | Record<string, any> = {}) {
+		this.apiConfig = new ApiConfig(options)
+		this.httpClient = new HttpClient(this)
+		this.transaction = new Transaction(this)
+	}
 
-// 	/**
-// 	 * Wrapper function that call `createTransaction` then:
-// 	 * @return {Promise} - Promise of String token
-// 	 */
-// 	createTransactionToken(parameter = {}) {
-// 		return this.createTransaction(parameter).then(function (res) {
-// 			return res.token
-// 		})
-// 	}
+	/**
+	 * Do `/transactions` API request to Snap API
+	 * @param parameter - object of Core API JSON body as parameter, will be converted to JSON (more params detail refer to: https://snap-docs.midtrans.com)
+	 * @return {Promise} - Promise contains Object from JSON decoded response
+	 */
 
-// 	/**
-// 	 * Wrapper function that call `createTransaction` then:
-// 	 * @return {Promise} - Promise of String redirect_url
-// 	 */
-// 	createTransactionRedirectUrl(parameter = {}) {
-// 		return this.createTransaction(parameter).then(function (res) {
-// 			return res.redirect_url
-// 		})
-// 	}
-// }
+	public createTransaction<T extends TransactionRequestType>(
+		parameter: T | Record<any, any> = {}
+	): Promise<Record<string, any>> {
+		const apiUrl = this.apiConfig.getSnapApiBaseUrl() + '/transactions'
+		const responsePromise = this.httpClient.request({
+			requestUrl: apiUrl,
+			httpMethod: 'post',
+			serverKey: this.apiConfig.get().serverKey,
+			requestPayload: parameter
+		})
+		return responsePromise
+	}
+
+	/**
+	 * Wrapper function that call `createTransaction` then:
+	 * @return {Promise} - Promise of String token
+	 */
+
+	public createTransactionToken<T extends TransactionRequestType>(
+		parameter: T | Record<any, any> = {}
+	): Promise<string> {
+		return this.createTransaction(parameter).then((res) => res.token)
+	}
+
+	/**
+	 * Wrapper function that call `createTransaction` then:
+	 * @return {Promise} - Promise of String redirect_url
+	 */
+
+	public createTransactionRedirectUrl<T extends TransactionRequestType>(
+		parameter: T | Record<any, any> = {}
+	): Promise<string> {
+		return this.createTransaction(parameter).then((res) => res.redirect_url)
+	}
+}
---
+++ src/types/snap.ts
@@ -0,0 +1,410 @@
+/*
+ *========================================
+ * @description Snap Options
+ *=======================================
+ */
+
+export interface SnapOptions {
+	readonly isProduction: boolean
+	readonly serverKey: string
+	readonly clientKey: string
+}
+
+/*
+ *========================================
+ * @description Global Charge Interface
+ *=======================================
+ */
+
+type TransactionDetailOptions = {
+	readonly order_id: string
+	readonly gross_amount: number
+}
+
+type BillingAddressOptions = {
+	readonly first_name?: string
+	readonly last_name?: string
+	readonly emai?: string
+	readonly phone?: string
+	readonly address: string
+	readonly city: string
+	readonly postal_code: string
+	readonly country_code?: string
+}
+
+type ItemDetailsOptions = {
+	readonly id?: string
+	readonly price: number
+	readonly quantity: number
+	readonly name: string
+	readonly brand?: string
+	readonly category?: string
+	readonly merchant_name?: string
+	/**
+	 * @descript For BCA Klikpay
+	 */
+	readonly tenor?: number
+	readonly code_plan?: number
+	readonly mid?: number
+}
+
+type CustomerDetailOptions = {
+	readonly first_name: string
+	readonly last_name: string
+	readonly email: string
+	readonly phone: string
+	readonly billing_address?: BillingAddressOptions
+}
+
+type ShipingDetailOptions = {
+	readonly first_name: string
+	readonly last_name: string
+	readonly email: string
+	readonly phone: string
+	readonly billing_address?: BillingAddressOptions
+}
+
+type DynamicDescriptorOptions = {
+	readonly merchant_name: string
+	readonly city_name: string
+	readonly country_code: string
+}
+
+type InstallmentOptions = {
+	readonly required: boolean
+	readonly terms: Record<string, number[]>
+}
+/*
+ *=================================
+ * @description Credit Card Snap
+ *=================================
+ */
+type CreditCardOptions = {
+	readonly secure?: boolean
+	readonly bank: string
+	readonly channel?: string
+	readonly type?: string
+	readonly whitelist_bins?: string[]
+	readonly installment?: InstallmentOptions
+	readonly dynamic_descriptor?: DynamicDescriptorOptions
+}
+
+interface CreditCard {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+	readonly credit_card?: CreditCardOptions
+}
+
+/*
+ *========================================
+ * @description BCA Virtual Account Snap
+ *========================================
+ */
+
+type FreeTextOptions = {
+	readonly inquiry: Record<string, string>
+	readonly payment: Record<string, string>
+}
+
+type BcaVtOptions = {
+	readonly va_number: boolean
+	readonly sub_company_code?: string
+	readonly free_text?: FreeTextOptions
+}
+
+interface BcaVirtualAccount {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+	readonly bca_va?: BcaVtOptions
+}
+
+/*
+ *==========================================
+ * @description Permata Virtual Account Snap
+ *==========================================
+ */
+
+type PermataVTOptions = {
+	readonly va_number: boolean
+	readonly recipient_name?: string
+}
+
+interface PermataVirtualAccount {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+	readonly permata_va?: PermataVTOptions
+}
+
+/*
+ *==========================================
+ * @description BNI Virtual Account Snap
+ *==========================================
+ */
+
+type BniVTOptions = {
+	readonly va_number: boolean
+}
+
+interface BniVirtualAccount {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+	readonly bni_va?: BniVTOptions
+}
+
+/*
+ *==========================================
+ * @description BNI Virtual Account Snap
+ *==========================================
+ */
+
+type BriVTOptions = {
+	readonly va_number: boolean
+}
+
+interface BriVirtualAccount {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+	readonly bri_va?: BriVTOptions
+}
+
+/*
+ *==========================================
+ * @description Mandiri Virtual Account Snap
+ *==========================================
+ */
+
+interface MandiriVirtualAccount {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description Gopay Snap
+ *==========================================
+ */
+
+type GopayOptions = {
+	readonly enable_callback?: boolean
+	readonly callback_url?: string
+}
+
+interface Gopay {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+	readonly gopay?: GopayOptions
+}
+
+/*
+ *==========================================
+ * @description Klik BCA Snap
+ *==========================================
+ */
+
+interface KlikBCA {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description Klik BCA Snap
+ *==========================================
+ */
+
+interface BcaKlikPay {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description Cimb Klik Snap
+ *==========================================
+ */
+
+interface CimbKlik {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description Danamon Online Banking Snap
+ *==========================================
+ */
+
+interface DanamonOnlineBanking {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description BRI Epay Snap
+ *==========================================
+ */
+
+interface BriEpay {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description Indomaret Snap
+ *==========================================
+ */
+
+interface Indomaret {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description Alfamart Snap
+ *==========================================
+ */
+
+interface Alfamart {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description Akulaku Snap
+ *==========================================
+ */
+
+interface AkuLaku {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+}
+
+/*
+ *==========================================
+ * @description Akulaku Snap
+ *==========================================
+ */
+
+type ShopeePayOptions = {
+	readonly callback_url: string
+}
+
+interface ShopeePay {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+	readonly shopeepay?: ShopeePayOptions
+}
+
+/*
+ *==========================================
+ * @description Akulaku Snap
+ *==========================================
+ */
+
+type CallbacksOptions = {
+	readonly finish: string
+}
+
+type ExpiryOptions = {
+	readonly start_time: string
+	readonly unit: string
+	readonly duration: number
+}
+
+interface snapFull {
+	readonly transaction_details: TransactionDetailOptions
+	readonly item_details?: ItemDetailsOptions
+	readonly customer_details?: CustomerDetailOptions
+	readonly shipping_address?: ShipingDetailOptions
+	readonly enabled_payments?: string[]
+	readonly credit_card?: CreditCardOptions
+	readonly whitelist_bins?: string[]
+	readonly dynamic_descriptor?: DynamicDescriptorOptions
+	readonly bca_va?: BcaVtOptions
+	readonly bri_va?: BriVTOptions
+	readonly permata_va?: PermataVTOptions
+	readonly shopeepay?: ShopeePayOptions
+	readonly callbacks: CallbacksOptions
+	readonly expiry?: ExpiryOptions
+	readonly custom_field1?: string
+	readonly custom_field2?: string
+	readonly custom_field3?: string
+}
+
+/*
+ *========================================
+ * @description Snap Transaction Request
+ *========================================
+ */
+
+export interface TransactionRequestType {
+	readonly snapCreditCard: CreditCard
+	readonly snapFull?: snapFull
+	readonly snapBca?: BcaVirtualAccount
+	readonly snapPermata?: PermataVirtualAccount
+	readonly snapBni?: BniVirtualAccount
+	readonly snapBri?: BriVirtualAccount
+	readonly snapMandiri?: MandiriVirtualAccount
+	readonly snapGopay?: Gopay
+	readonly snapKlikBca?: KlikBCA
+	readonly snapBcaKlikPay?: BcaKlikPay
+	readonly snapCimbKlik?: CimbKlik
+	readonly snapDanamonOnline?: DanamonOnlineBanking
+	readonly snapBriEpay?: BriEpay
+	readonly snapIndomaret?: Indomaret
+	readonly snapAlfamart?: Alfamart
+	readonly snapAkuLaku?: AkuLaku
+	readonly snapShopePay?: ShopeePay
+}
