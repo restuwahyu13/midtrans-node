7c1def520548ca682034dbfefad40f94c8a91d7c
restuwahyu13 <restuwahyu13@gmail.com>
2021-01-17 23:25:36 +0700

    update jest.config to type ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,13 @@
-module.exports = {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	moduleFileExtensions: ['ts', 'js'],
 	testMatch: ['<rootDir>/test/**/*.{test.ts, spec.ts}', '<rootDir>/__test__/**/*.{test.ts, spec.ts}'],
-	// collectCoverageFrom: ['src/lib/**/*.ts'],
+	collectCoverageFrom: ['src/**/*'],
 	testPathIgnorePatterns: ['node_modules/', 'dist/', 'esm', 'tsconfig.json', 'coverage/'],
 	coveragePathIgnorePatterns: ['node_modules/', 'dist/', 'esm/', 'tsconfig.json', 'coverage/']
 }
+
+export default config
